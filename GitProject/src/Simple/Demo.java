fb0fe590dc21f5e68b0074ac813961bacf354954
vyankateshb02 <130127241+vyankateshb02@users.noreply.github.com>
2023-04-13 13:56:33 +0530

    Update Demo.java

--- GitProject/src/Simple/Demo.java
+++ GitProject/src/Simple/Demo.java
@@ -4,8 +4,8 @@ public class Demo {
 	
 	public static void main(String[] args) {
 		
-		System.err.println("Welcome to Pune");
-		system.println("Hello");
+		System.out.println("Welcome to cjc")
+		system.out.println("Hello");
 	}
 
 }
